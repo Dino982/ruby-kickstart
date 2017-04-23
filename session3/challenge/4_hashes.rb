31603efcebbd6c0db89f467dc2c5eb523f9430af
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-23 23:24:41 +0400

    Challenge 4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,18 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+  to_return = {}
+  str.split.each_with_index do |word, index|
+    to_return[word] ||= index
+  end
+  to_return
+end
+
+
+# nb   operator
+# a ||= b    is the same as   a || a = b
+
+# if 'a' is set and has some true value then it remains the same, 
+# otherwise  it takes the value of 'b'
